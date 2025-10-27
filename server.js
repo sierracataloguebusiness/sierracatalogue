4d1590eb90eb90fda918eb343c37cfea09544430
sierracataloguebusiness <sierracatalogue.business@gmail.com>
2025-10-27 23:16:46 +0000

    CORS fix

--- server.js
+++ server.js
@@ -30,9 +30,14 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-    origin: ['http://localhost:5173', 'https://sierra-catalogue.onrender.com'],
+    origin: [
+        'http://localhost:5173',
+        'https://sierra-catalogue.onrender.com',
+        'https://www.sierracatalogue.com'
+    ],
     credentials: true,
 }));
+
 app.use(express.json());
 
 // API Routes
